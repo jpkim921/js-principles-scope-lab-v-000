9c9c0688c0d62e6595ea2aa249a9e3ce66c75a4e
Jayon Kim <jpkim921@gmail.com>
2018-09-27 04:13:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var customerName = 'bob';
 
-function upperCaseCustomerName(customerName){
+function upperCaseCustomerName(){
   // console.log(customerName);
   return customerName.toUpperCase();
 }
